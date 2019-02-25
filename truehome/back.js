735c6990fd3989c315939fa92335406c7f2966d6
djkhan20 <37348103+djkhan20@users.noreply.github.com>
2019-02-24 19:43:12 -0600

    TRUE HOME PROPIEDADES

---
+++ truehome/back.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const propiedades = [
+{
+	id: '0',
+	nombre: 'Propiedad 1',
+	direccion: 'av1',
+	dueño: 'dueño1',
+	costo: '1000'
+},
+{
+	id: '1',
+	nombre: 'Propiedad 2',
+	direccion: 'av2',
+	dueño: 'dueño2',
+	costo: '2000'
+},
+{
+	id: '2',
+	nombre: 'Propiedad 3',
+	direccion: 'av3',
+	dueño: 'dueño3',
+	costo: '3000'
+},
+{
+	id: '3',
+	nombre: 'Propiedad 4',
+	direccion: 'av4',
+	dueño: 'dueño4',
+	costo: '4000'
+},
+{
+	id: '4',
+	nombre: 'Propiedad 5',
+	direccion: 'av5',
+	dueño: 'dueño5',
+	costo: '5000'
+}
+];
+
+
+function actualizarPropiedad(clave,nombreProp, nombre, direccion, costo){
+  console.log(clave);
+  console.log(nombre);
+  console.log(direccion);
+    for (var i = 0; i < propiedades.length; i++) {
+        if (propiedades[i].id == clave) {
+        	propiedades[i].nombre = nombreProp;
+            propiedades[i].dueño = nombre;
+            propiedades[i].direccion = direccion; 
+            propiedades[i].costo = costo; 
+            break;
+        }
+    }
+//console.log(propiedades);
+}
+
+function guardarPropiedad(nombreProp, nombreDuen, direccion, costo){
+	propiedades.push({
+		id: Math.random(),
+		nombre: nombreProp,
+		dueño: nombreDuen,
+		direccion: direccion,
+		costo: costo
+	});
+}
+
+app.get('/propiedades', (req, res) => {
+  res.json(propiedades);
+});
+
+
+app.put('/propiedades/:id', function(req, res, next) {
+    var id = req.params.id;
+    //console.log("id en api: " +id);
+	//console.log("texto enviado en api: "+ req.body.post);
+    actualizarPropiedad( req.params.id , req.body.nombreProp ,req.body.nombre, req.body.direccion, req.body.costo);
+	res.send('Actualizado');
+});
+
+app.post('/propiedades/new',(req,res)=>{
+  guardarPropiedad(req.body.nombreProp, req.body.nombre, req.body.direccion, req.body.costo);
+  res.send('Creado');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ truehome/front/src/App.js
@@ -0,0 +1,166 @@
+import React, { Component } from 'react';
+import './App.css';
+import Modal from 'react-responsive-modal';
+import './Estilos/Modal.css';
+
+class App extends Component {
+  state = {
+    propiedades: [],
+    open: false,
+    idCamb:'',
+    nombrePropCamb:'',
+    nombreCamb:'',
+    direcCamb: '',
+    costCamb: '',
+    nombreNu:'',
+    direccionNu: '',
+    duenoNu:'',
+    costoNu:''
+  };
+
+  componentDidMount() {
+    this.traerProp()
+      .then(res => this.setState({ propiedades: res }))
+      .catch(err => console.log(err));
+  }
+
+  openModal (id,nombreProp, nombre,direccion, costo) {
+    this.setState({ open: true });
+    this.setState ({idCamb: id, nombrePropCamb: nombreProp, nombreCamb: nombre, direcCamb: direccion, costCamb: costo});
+  };
+
+  closeModal = () => {
+    this.setState({ open: false });
+  };
+
+  traerProp = async () => {
+    const proper = await fetch('/propiedades');
+    const body = await proper.json();
+    console.log(body);
+    if (proper.status !== 200) throw Error(body.message);
+    return body;
+  };
+
+  edicionProp = async e => {
+    const id = this.state.idCamb;
+    const response = await fetch('/propiedades/' + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ nombreProp: this.state.nombrePropCamb, nombre: this.state.nombreCamb, direccion: this.state.direcCamb, costo: this.state.costCamb }),
+    });
+    const actual = await response.text();
+    console.log (actual);
+  };
+
+  nuevaProp = async e => {
+    const response = await fetch('/propiedades/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ nombreProp: this.state.nombreNu , nombre: this.state.duenoNu, direccion: this.state.direccionNu, costo: this.state.costoNu }),
+    });
+    const actual = await response.text();
+    console.log (actual);
+  }
+
+render() {
+  const {open} = this.state;
+    return (
+      <div className='app'>
+          <header>
+            <div className='wrapper'>
+              <h1>VISOR TRUEHOME</h1>
+            </div>
+          </header>         
+          <div className='container'>
+            <section className='add-item'>
+                <strong>Nueva Propiedad</strong>
+                <form style = {{marginTop: '20px'}} onSubmit = {this.nuevaProp}>
+                  <input type="text" name="currentItem" placeholder="Propiedad" onChange={e => this.setState({ nombreNu: e.target.value })} />
+                  <input type="text" name="currentItem" placeholder="Dueño" onChange={e => this.setState({ duenoNu: e.target.value })} />
+                  <input type="text" name="currentItem" placeholder="Dirección" onChange={e => this.setState({ direccionNu: e.target.value })} />
+                  <input type="text" name="currentItem" placeholder="Costo" onChange={e => this.setState({ costoNu: e.target.value })} />
+                  <button>Agregar</button>
+                </form>
+            </section>
+            <section className='display-item'>
+              <div className='wrapper'>
+                <ul>
+                  {this.state.propiedades.map((step, index) => (
+                    <li key={step.id} >
+                      <h3>{step.nombre}</h3>
+                      <p>Cliente: {step.dueño}</p>
+                      <p>Dirección: {step.direccion}</p>
+                      <p>Costo: {step.costo}</p>
+                      <button onClick = {(e)=> this.openModal(step.id, step.nombre, step.dueño, step.direccion, step.costo)} >Editar</button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </section>
+          </div>
+    <Modal 
+      open={open} 
+      onClose={this.closeModal} 
+      classNames={{
+        overlay: 'custom-overlay', 
+        modal: 'custom-modal',
+        transitionEnter: 'transition-enter',
+        transitionEnterActive: 'transition-enter-active',
+        transitionExit: 'transition-exit-active',
+        transitionExitActive: 'transition-exit-active',
+        closeButton : 'closeButton' 
+      }}
+      animationDuration={300}
+      center
+      >
+        <form onSubmit={this.edicionProp}>
+          <h3>Editar</h3>
+          <p>
+            <strong>Propiedad:</strong>
+          </p>
+          <input
+          required
+          type="text"
+          defaultValue={this.state.nombrePropCamb}
+          onChange={e => this.setState({ nombrePropCamb: e.target.value })}
+          />
+          <p>
+            <strong>Cliente:</strong>
+          </p>
+          <input
+          required
+          type="text"
+          defaultValue={this.state.nombreCamb}
+          onChange={e => this.setState({ nombreCamb: e.target.value })}
+          />
+          <p>
+            <strong>Direccion</strong>
+          </p>
+          <input
+          required
+          type="text"
+          defaultValue={this.state.direcCamb}
+          onChange={e => this.setState({ direcCamb: e.target.value })}
+          />
+          <p>
+            <strong>Costo:</strong>
+          </p>
+          <input
+          required
+          type="text"
+          defaultValue={this.state.costCamb}
+          onChange={e => this.setState({ costCamb: e.target.value })}
+          />
+          <button type="submit">Cambiar</button>
+        </form>
+    </Modal>
+      </div>
+    );
+  }
+}
+
+export default App;
